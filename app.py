1c150deb30c777bf4ed6158d9732c11f6365a746
Jacob Gallop <gallop.jacob@gmail.com>
2025-01-30 08:39:01 +0800

    Removed gemini sdk from app, replaced with raw api request

--- app.py
+++ app.py
@@ -1,41 +1,20 @@
-import google.generativeai as genai
 from flask import Flask, render_template, jsonify
+import requests
 import os
-from typing_extensions import TypedDict
-import json
 
 app = Flask(__name__)
 
 API_KEY = os.environ.get("GEMINI_API_KEY")
-genai.configure(api_key=API_KEY)
 
 instruction = """
 You are a journalist for a tech article which posts about the latest tech news.
 The article caters to engineers, and others alike with a similar context.
 """
 
-class Article(TypedDict):
-    title: str
-    summary: str
-    source: str
-
 def fetch_tech_news():
-    model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=instruction)
-    
-    gen_cfg = genai.GenerationConfig(
-        response_mime_type="application/json", response_schema=list[Article]
-    )
+    # make API request for news in JSON
 
-    try:
-        response = model.generate_content(
-            "List 10 articles which outline the tech-related events of the last month.",
-            generation_config=gen_cfg,
-        )
-
-        news = json.loads(response.text)
-    except:
-        news = []
-
+    news = []
     return news
 
 @app.route("/")
